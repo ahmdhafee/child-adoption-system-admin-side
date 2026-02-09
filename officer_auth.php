62811d2aaf5bfccb3822534b93159b9b741cf8c5
ahamedhafee <ahamedhafeel29@gmail.com>
2026-02-09 08:40:03 +0530

    second commit

--- officer_auth.php
+++ officer_auth.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 session_start();
 
-// Officer must be logged in
+
 if (!isset($_SESSION['officer_id'], $_SESSION['officer_role'])) {
     header("Location: .\officer_login.php?error=unauthorized");
     exit;
 }
 
-// Optional: session timeout (30 minutes)
+
 $timeout = 30 * 60;
 
 if (isset($_SESSION['login_time']) && (time() - $_SESSION['login_time']) > $timeout) {
@@ -18,5 +18,5 @@ if (isset($_SESSION['login_time']) && (time() - $_SESSION['login_time']) > $time
     exit;
 }
 
-// Refresh activity time
+
 $_SESSION['login_time'] = time();
